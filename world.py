ccc361d0215fe573601690065080a2bd45a62511
jasoncwilley <jaycw0001@gmail.com>
2016-09-11 09:20:37 -0400

    cleaned up world.py and fixed victory tile so the game ends

--- world.py
+++ world.py
@@ -19,17 +19,16 @@ class StartTile(MapTile):
     def intro_text(self):
 # Text below will populate the StartTile variable intro-text
         return """
-********************************************************************************
-Welcome to Choice City--the gateway to Enlightenment.  The streets of Choice\n 
-City are filled what you would expect given the name, Choices.  These Choices as\n 
-well as the ability to learn from your expierences will be critical to the sucess\n
-of your journey.
-********************************************************************************
-                                   
-
-      *******************************************************************
-      ********"The unexamined life is not worh living." --Socrates*******
-      *******************************************************************
+**************************************************************************** 
+* Welcome to Choice City--the gateway to Enlightenment.  The streets of    *
+* Choice *City are filled what you would expect given the name, Choices.   *
+* These Choices as well as the ability to learn from your expierences will *
+* be critical to the sucess of your journey.                               *  
+****************************************************************************
+                            
+*******************************************************************
+********"The unexamined life is not worh living." --Socrates*******
+*******************************************************************
 """
 
 
@@ -44,28 +43,28 @@ class EnemyTile(MapTile):
             self.enemy = enemies.Wolves()
 # sets .alive_text             
             self.alive_text = """
-********************************************************************************************
-Out of nowhere you find yourself surrounded by a pack of vicious hater wolves.  These wolf 
-like creatures feast on insecurities and would like nothing more than to eviscerate your 
-selfconfidence. 
-*******************************************************
-             ****************************************************************   
-             *   *Not Today!  Your Attack seems to be working. Nice move!*  * 
-             ****************************************************************                  
-             ****************************************************************                 
-         *********************************************************************** 
-         *  **"Dout will kills more dreams than failure ever will." --Kassem** *
-         ***********************************************************************
+***********************************************************************
+*  Out of nowhere you find yourself surrounded by a pack of vicious   *
+*  hater wolves.  These wolf like creatures feast on insecurities and *
+*  would like nothing more than to eviscerate your selfconfidence.    *
+***********************************************************************
 
-         """
+****************************************************************** 
+**"Dout will kills more dreams than failure ever will." --Kassem** 
+******************************************************************
+"""
             self.dead_text =  """
-         ********************************************************************************   
-         *   The wolves proved to be stronger than you thought.  You were not able to    *
-         *   reach the the Magical Forest on the other side of the City.  Learning from  *
-         *   your mistakes will put your on the fast track to Enlightenment and if you   *
-         *   can apply thesel lessons in your next life you be more likely to suceed.    *                                                     ********************************************************************************
-         *     **"Doubt kills more dreams than failure ever will." --Kassem          * 
-         *********************************************************************************
+********************************************************************   
+* The wolves proved to be stronger than you thought.  You were     * 
+* not able to reach the the Magical Forest on the other side of    *
+* the City.  Learning from  your mistakes will put your on the     *
+* fast track to Enlightenment and if you can apply thesel lessons  *
+* in your next life you be more likely to suceed.                  *
+********************************************************************
+
+***************************************************************
+**"Doubt kills more dreams than failure ever will." --Kassem **
+***************************************************************
 """
 
 #if e ia < .0.75 hen select Ogre for the user to face and .alive_text
@@ -73,66 +72,82 @@ selfconfidence.
 #if r is lessthank            
             self.enemy = enemies.Ogre()
             self.alive_text = """
-     ***********************************************************************************************
-     * An ogre sized ego is blocking your path!Egos that have grown to the ize of this the Ogre's  * 
-     * can do only one thing! Well 2 things, to be correct all the time and keeping you from       *
-     * achieving enlightment Learning to apologize does not mean you are wrong and another person  *
-     * is right. Respecting other's beliefs even when they go against your own may not easy but    *
-     * hey!, Do you want to be right or happy? The choice is yours.                                *
-     * *********************************************************************************************
+**************************************************************************
+* An ogre sized ego is blocking your path!Egos that have grown to the    *  
+* size of this the Ogre's can do only one thing! Well 2 things, to be    * 
+* correct all the time and keeping you from achieving enlightment        *
+* Learning to apologize does not mean you are wrong and another person   *
+* is right. Respecting other's beliefs even when they go against your    * 
+* own may not easy but Hey!, Do you want to be right or happy? The       *
+* choice is yours.                                                       *
+**************************************************************************
 """
             self.dead_text= """ 
-             ******************************************************************************
-             *        Wow! You were able to check that ogre's ego with tact and class.    *
-             *     Choices like that will surely lead you to the other side of the City.  *
-             ******************************************************************************
-            *********************************************************************************
-            * The Ancients say 'Yeild to overcome; Bend and be straight; Empty and be full; *
-            *   Wear out to become new; Have little and gain; Have much and be confused...  * 
-            ********************************************************************************* 
+*************************************************************************
+*   Wow! You were able to check that ogre's ego with tact and class.    *
+*  Choices like that will surely lead you to the other side of the City.* 
+*************************************************************************
+
+********************************************************************
+* The Ancients say 'Yeild to overcome; Bend and be straight;       *    
+* Empty and be full; Wear out to become new; Have little and gain; *
+* Have much and be confused...                                     *
+******************************************************************** 
 """
                                
         elif r < 0.85:
             self.enemy = enemies.KarmaChameleon()
             self.alive_text = """
-
-         *********************************************************************************
-         *  You feel the wind pick up and you are suddenly lost in swarm of bommerangs!  *
-         *  You quickly notice that these are not your average everyday boomerangs but   * 
-         *  instead one's that you threw in the distant past.  Everyone has a past or a  * 
-         *  history of Choices and if this has happend to you while on your journey      *
-         *  through the city take a breath and deal with your karma now because by it's  * 
-         *  very nature it's not coming back to or for you.  Hopefully the former!"      *
-         *********************************************************************************
+***********************************************************************
+* You feel the wind pick up and you are suddenly lost in swarm of     *
+* bommerangs!  You quickly notice that these are not your average     *  
+* everyday boomerangs but instead one's that you threw in the         * 
+* distant past.  Everyone has a past or a history of Choices and      *
+* if this has happend to you while on your journey through the city   *
+* take a breath and deal with your karma now because by it's very     *
+* nature it's not coming back to or for you.  Hopefully the former!   *
+***********************************************************************
 """
-            self.dead_text =  """
-                 ************************************************************************** 
-                 * Dozens of boomerangs have hit your and knocked you to the ground.      *
-                 * Look on the bright side you were able to settle an recent or age old   * 
-                 * karmatic debt.                                                         *
-                 **************************************************************************  
-            **********************************************************
-            *  'There is such thing as good or bad, right or wrong,  *
-            *    those are perspectives of what actual is' --Tolle   * 
-            **********************************************************     
+
+            self.dead_text =  """                                                         
+************************************************************************** 
+* Dozens of boomerangs have hit your and knocked you to the ground.      *
+* Look on the bright side you were able to settle an recent or age old   * 
+* karmatic debt.                                                         *
+**************************************************************************  
+**********************************************************
+*  "There is such thing as good or bad, right or wrong,  *
+*    those are perspectives of what actual is" --Tolle   * 
+**********************************************************
 """                              
+
         else:
            self.enemy = enemies.KarmaChameleon()
-           self.alive_text = """
-
-You feel the wind pick up and you are suddenlylost in swarm of bommerangs!  
-You quickly notice that these are not your average everyday boomerangs but 
-instead one's that you threw in the distant past.  Everyone has a past or 
-a history of Choices and if this has happend to you while on your journey 
-through the city take a breath and deal with your karma now because by 
-it's very nature it's not coming back to or for you. Hopefully the former!
+           self.alive_text = """                                                                                                                                                   
+*********************************************************************
+* You feel the wind pick up and you are suddenlylost in swarm of    *
+* bommerangs! You quickly notice that these are not your average    *
+* everyday boomerangs but instead one's that you threw in the       *    
+* distant past.  Everyone has a past or a history of Choices and    *   
+* if this has happend to you while on your journeythrough the city  *
+* take a breath and deal with your karma now because by  it's very  *
+* nature it's not coming back to or for you. Hopefully the former!  *
+*********************************************************************
 """
 
+
            self.dead_text =  """
-           Dozens of boomerangs have hit your and knocked you to the ground.  Look on the 
-           bright side you were able to settle an recent or age old karmatic debt.
-           'There is such thing as good or bad, right or wrong, those are perspectives of
-           what actual is' --Tolle """
+*********************************************************************
+* Dozens of boomerangs have hit your and knocked you to the ground. * 
+* Look on the bright side you were able to settle an recent or age  *
+* old karmatic debt.                                                *
+*********************************************************************
+
+******************************************************************
+*"There is such thing as good or bad, right or wrong, those are  * 
+* perspectives of what actual is' --Tolle                        * 
+******************************************************************
+"""
         super().__init__(x, y) #Defines the intro_text Player will see when they enter room
 
     def intro_text(self):
@@ -152,12 +167,27 @@ class VictoryTile(MapTile):
 
     def intro_text(self):
         return """
-        You see a bright light in the distance...
-        ... it grows as you get closer! It's sunlight!
+****************************************************************************
+* As you approach the far end of the City you stop to gather your          *
+* thoughts and appreciate the moment.  As you turn down the road labeled   *
+* Destiny Lane you see a structure a beautiful palace in the distance.     *
+* Excited youwalk faster and begin to run.  Enlightment is in your site    *
+* and youare determinded to get there before night fall.  As the sun goes  *
+* downyou keep walking towards the mystical destination.  Late into the    *  
+* night you are exhuausted and have to stop to sleep.You tell yourself     *
+* It has to be close.  When the sun rises you jump up and quickly notice   *
+* that you are not any closer to the destination than you were the day     *
+* before.  At this point you decide to turn around because the realization *
+* that Enlightment is a process not a destination.  How far you go down    *
+* Destiny Lane is exactly how fare you go down Destiny Lane, if you even   *
+* make it there, depends on you.                                           *
+****************************************************************************
 
-
-        Victory is yours!
-        """
+**********************************************
+* Victory Is Yours! or Maybe This is Defeat? *
+*   Like everything else the Choice is Yours!*
+**********************************************
+"""
 
 
 class FindGoldTile(MapTile):
@@ -175,12 +205,18 @@ class FindGoldTile(MapTile):
     def intro_text(self):
         if self.gold_claimed:
             return """
-            Another unremarkable part of the cave. You must forge onwards.
-            """
+*****************************************************
+* The alley you choose to go down is quiet and dark *
+* You forge onwards.                                *
+*****************************************************
+"""
         else:
             return """
-            Someone dropped some gold. You pick it up.
-            """
+*******************************************************
+* Your good Karma is paying off, literally.  You find *
+* some gold on the ground                             *
+*******************************************************            
+"""
 
 #
 #class TraderTile(MapTile):
@@ -230,14 +266,17 @@ class FindGoldTile(MapTile):
 
     def intro_text(self):
         return """
-        A frail not-quite-human, not-quite-creature squats in the corner
-        clinking his gold coins together. He looks willing to trade.
-        """
+*******************************************************
+*  Out of the corner of you eye you see a purse full  * 
+*  of gold.  Your inner guide senses danger so you    *      
+*  decide to ignore it and forge on.                  *
+*******************************************************
+"""
 
 world_dsl = """
 |EN|EN|VT|EN|EN|
-|EN|  |  |  |EN|
-|EN|FG|EN|  |FG|
+|FG|  |EN|  |EN|
+|FG|FG|EN|  |FG|
 |FG|  |ST|FG|EN|
 |FG|  |EN|  |FG|
 """
